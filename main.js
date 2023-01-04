db69f8102f8cfd69a97c8bb6c2a10934270e4d32
Seohong Kim <greyxcx@gmail.com>
2023-01-05 01:07:54 +0900

    Add restart button

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+const section = document.querySelector('section');
 const header = document.querySelector('header');
 const playBtn = document.querySelector('.playBtn');
 const stopBtn = document.querySelector('.stopBtn');
+const restart = document.querySelector('.restart');
 const timer = document.querySelector('.timer');
 const time = { };
 
@@ -13,7 +15,18 @@ function counter() {
     time.interval = setInterval(() => {
         timer.innerHTML = second;
         second--;
+        
+        if(second < 0) {
+            clearInterval(time.interval);
+        };
+    }, 1000);
+};
 
+playBtn.addEventListener("click", () => {
+    if (playBtn.className === 'playBtn') {
+        playBtn.classList.replace('playBtn', 'stopBtn');
+        playBtn.innerHTML = `<i class="fa-solid fa-stop"></i>`;
+        
         let carrotHeight = Math.floor(Math.random() * 300);
         let carrotWidth = Math.floor(Math.random() * 1100);
         let bugHeight = Math.floor(Math.random() * 300);
@@ -23,24 +36,20 @@ function counter() {
         carrot.style.left = `${carrotWidth}px`;
         bug.style.top = `${bugHeight}px`;
         bug.style.left = `${bugWidth}px`;
-        
-        if(second < 0) {
-            clearInterval(time.interval);
-        };
-    }, 1000);
-};
 
-
-
-playBtn.addEventListener("click", () => {
-    if (playBtn.className === 'playBtn') {
-        playBtn.classList.replace('playBtn', 'stopBtn');
-        playBtn.innerHTML = `<i class="fa-solid fa-stop"></i>`;
         counter();
-    } else {
+        
+    } else if (playBtn.className === 'stopBtn') {
         clearInterval(time.interval);
-        playBtn.classList.replace('stopBtn', 'playBtn');
-        playBtn.innerHTML = `<i class="fa-solid fa-play"></i>`;
+        playBtn.style.visibility = 'hidden';
+        
+        const footer = document.createElement('footer');
+        section.appendChild(footer);
+        footer.innerHTML = `<button class="restart" type="button"><i class="fa-solid fa-rotate-right"></i></button>
+        <span class="result">YOU LOST</span>`;
     };
 });
 
+$(document).on("click", 'restart', () => {
+
+});
